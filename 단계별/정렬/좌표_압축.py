9a620ddf027b146607b0b99d76e296556db4c4ee
yejipractice <twoye1193@ajou.ac.kr>
2021-08-31 20:44:22 +0900

    [FIX] 백준 18807번 _다시해보기

--- 단계별/정렬/좌표_압축.py
+++ 단계별/정렬/좌표_압축.py
@@ -7,7 +7,14 @@ nums = list(map(int, input().split()))
 
 ranks = sorted(list(set(nums)))
 
-for i in nums:
-    print(ranks.index(i), end=" ")
+dics = {}
 
-# 시간초과
+# 딕셔너리 사용법
+for i in range(len(ranks)):
+    dics[ranks[i]] = i
+
+for i in range(N):
+    nums[i] = dics[nums[i]]
+
+# 배열 값만 출력
+print(*nums)
