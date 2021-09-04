cca27220b3586e194b786b9d97482ff4c73bb028
yejipractice <twoye1193@ajou.ac.kr>
2021-09-04 17:04:49 +0900

    [FIX] 백준 17298번 _다시해보기

--- 알고리즘_기초/자료구조1/오큰수.py
+++ 알고리즘_기초/자료구조1/오큰수.py
@@ -4,14 +4,26 @@ input = sys.stdin.readline
 N = int(input())
 
 nums = list(map(int, input().split()))
+nums.reverse()
+container = []
 
 for i in range(N):
-    num = nums[i]
-    answer = -1
-    for j in range(i+1, N):
-        if nums[j] > num:
-            answer = nums[j]
+    num = nums.pop()
+    while(True):
+        if len(nums) == 0:
+            print(-1, end=" ")
+            for _ in range(len(container)):
+                nums.append(container.pop())
             break
-    print(answer, end=" ")
+        else:
+            next = nums.pop()
+            if next > num:
+                print(next, end=" ")
+                nums.append(next)
+                for _ in range(len(container)):
+                    nums.append(container.pop())
+                break
+            else:
+                container.append(next)
 
-# 시간 초과
+# 스택을 이용한 두 번째 시도 -> 시간 초과
