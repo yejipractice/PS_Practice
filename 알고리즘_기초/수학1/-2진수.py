2c2f0abe73624c9a845aed6f2c01800f2989444b
yejipractice <twoye1193@ajou.ac.kr>
2021-09-12 17:57:12 +0900

    [ADD 백준 2089번 _다시해보기

---
+++ 알고리즘_기초/수학1/-2진수.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+res = ''
+if n == 0:
+    res = 0
+while n:
+    if n % (-2):
+        res = "1"+res
+        n = n//-2 + 1
+    else:
+        res = '0'+res
+        n //= -2
+
+print(res)
