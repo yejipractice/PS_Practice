4129d01a6e9d2942203dd8b41b8f021b3a907ae3
yejipractice <twoye1193@ajou.ac.kr>
2021-09-22 13:45:19 +0900

    [ADD] 백준 1476번

---
+++ 알고리즘_기초/브투프_포스/날짜_계산.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+e, s, m = map(int, input().split())
+
+year = 1
+while 1:
+    if (year-e) % 15 == 0 and (year-s) % 28 == 0 and (year-m) % 19 == 0:
+        print(year)
+        break
+    year += 1
