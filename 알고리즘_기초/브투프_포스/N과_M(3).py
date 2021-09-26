ad4c620b167a10ca7b2c46039cc6834825ffca49
yejipractice <twoye1193@ajou.ac.kr>
2021-09-26 23:08:13 +0900

    [ADD] 백준 15651번 _다른풀이 _다시해보기

--- 알고리즘_기초/브투프_포스/N과_M(3).py
+++ 알고리즘_기초/브투프_포스/N과_M(3).py
@@ -1,13 +1,18 @@
-from itertools import product, repeat
-# 중복 순열 모듈
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-nums = [i for i in range(1, n+1)]
+s = []
 
 
-res = product(nums, repeat=m)
+def dfs():
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+    for i in range(1, n+1):
+        s.append(i)
+        dfs()
+        s.pop()
 
-for i in res:
-    print(*i)
+
+dfs()
