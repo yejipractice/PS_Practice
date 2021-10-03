4c33c6977db27593a016a2cccc0306200cd57a0a
yejipractice <twoye1193@ajou.ac.kr>
2021-10-03 14:16:10 +0900

    [ADD] 백준 15652번 _다시해보기

--- 알고리즘_기초/브루트_포스/N과_M(4).py
+++ 알고리즘_기초/브루트_포스/N과_M(4).py
@@ -1,18 +1,20 @@
-from itertools import product
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-nums = [i for i in range(1, n+1)]
 
-a = product(nums, repeat=m)
-answer = []
+s = []
 
-for i in a:
-    i = list(i)
-    i.sort()
-    if i not in answer:
-        answer.append(i)
 
-for i in answer:
-    print(*i)
+def dfs(start):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(start, n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+
+
+dfs(1)
