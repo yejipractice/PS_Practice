47624fe684525eafce743ad4bed1c576ebc200b7
yejipractice <twoye1193@ajou.ac.kr>
2022-01-07 18:39:52 +0900

    [FIX] SWExpert 5207번 _다시해봄

--- SW_EXPERT/Advanced_Learn/이진 탐색.py
+++ SW_EXPERT/Advanced_Learn/이진 탐색.py
@@ -1,31 +1,47 @@
 for tc in range(1, int(input())+1):
+    answer = 0
     n, m = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
     a.sort()
-    answer = 0
-    for i in range(m):
+    for bb in b:
         start = 0
-        end = n-1
-        
-        tar = b[i]
+        end = len(a)-1
         history = 0
-        while start<=end:
+        while start <= end:
             mid = (start+end)//2
-            if tar == a[mid]:
+            if bb == a[mid]: #  # 연속 같은 방향이 발생하지 않았으면서 값 존재 => 조건 성립
                 answer +=1
                 break
-            elif tar > a[mid]:
-                if history == 2:
+            if bb>= a[mid]:
+                if history == 1: # 연속 같은 방향 
                     break
                 else:
                     start = mid+1
-                    history =2
-            elif a[mid] > tar:
-                if history == 1:
+                    history = 1
+            else:
+                if history == 2:  # 연속 같은 방향
                     break;
                 else:
-                    end = mid -1
-                    history = 1
-    print("#{} {}".format(tc, answer))
-        
+                    end = mid-1
+                    history = 2
+    print(answer)
+            
+
+'''
+입력
+3
+3 3
+1 2 3
+2 3 4
+3 5
+1 3 5
+2 4 6 8 10
+5 5
+1 3 5 7 9
+1 2 3 4 5
+출력
+#1 2
+#2 0
+#3 3
+'''
