14ccd19e1fa4f6d414f240b6b36050ea194d1d4e
yejipractice <twoye1193@ajou.ac.kr>
2022-01-07 17:52:26 +0900

    [FIX] SWExpert 5204번 _다시해봄

--- SW_EXPERT/Advanced_Learn/병합 정렬.py
+++ SW_EXPERT/Advanced_Learn/병합 정렬.py
@@ -1,38 +1,58 @@
-def merge_arr(left_arr, right_arr):
+def merge(left, right):
     global answer
-    if left_arr[-1] > right_arr[-1]:
-        answer += 1
-    merge_arr = []
-    left_idx, right_idx = 0, 0
-    n, m = len(left_arr), len(right_arr)
-
-    while left_idx != n and right_idx != m:
-        if left_arr[left_idx] <= right_arr[right_idx]:
-            merge_arr.append(left_arr[left_idx])
-            left_idx += 1
+    array = []
+    
+    if left[-1] > right[-1]:
+        answer+=1
+    
+    n = len(left)
+    m = len(right)
+    
+    lidx, ridx = 0,0;
+    
+    while lidx != n and ridx != m:
+        if left[lidx] <= right[ridx]:
+            array.append(left[lidx])
+            lidx+=1
         else:
-            merge_arr.append(right_arr[right_idx])
-            right_idx += 1
+            array.append(right[ridx])
+            ridx+=1
+        
+    if lidx != n:
+        array+=left[lidx:]
+    if ridx != m:
+        array+=right[ridx:]
+    return array
 
-    if left_idx != n:
-        merge_arr += left_arr[left_idx:]
-    if right_idx != m:
-        merge_arr += right_arr[right_idx:]
+def merge_sort(list):
+    if len(list) <=1:
+        return list;
+    
+    mid = len(list)//2
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+    
+    return merge(left, right)
 
-    return merge_arr
-
-def divide_arr(a):
-    if len(a) == 1:
-        return a
-    mid = len(a) // 2
-    left = divide_arr(a[:mid])
-    right = divide_arr(a[mid:])
-    return merge_arr(left, right)
-
-
-for test in range(1, int(input()) + 1):
-    N = int(input())
-    arr = list(map(int, input().split()))
+for tc in range(1, int(input())+1):
+    n = int(input())
+    nums = list(map(int, input().split()))
     answer = 0
-    arr = divide_arr(arr)
-    print(f'#{test} {arr[N//2]} {answer}')
+    merged = merge_sort(nums)
+    result = merged[len(merged)//2]
+    print("#"+str(tc), end=" ")
+    print(result, answer)
+   
+   
+# linkedlist로 했을 때 시간 초과가 났다. 인덱스로 접근하는 것이 더 빨라서 그런가보다.  
+'''
+입력
+2
+5
+2 2 1 1 3
+10
+7 5 4 1 2 10 3 6 9 8
+출력
+#1 2 0
+#2 6 6
+'''
